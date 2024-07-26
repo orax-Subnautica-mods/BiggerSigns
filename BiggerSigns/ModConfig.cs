6a8a52337aa2f83746681ce1494096a523f9f460
orax <0-F@users.noreply.github.com>
2024-07-26 14:14:17 +0200

    init

---
+++ BiggerSigns/ModConfig.cs
@@ -0,0 +1,26 @@
+﻿using Nautilus.Json;
+using Nautilus.Options.Attributes;
+
+namespace BiggerSigns
+{
+    [Menu(PluginInfo.PLUGIN_NAME)]
+    public class ModConfig : ConfigFile
+    {
+        public const int SCALE_MIN = -3;
+        public const int SCALE_MAX = 3;
+        public const float SCALE_STEP = 0.0002f;
+        public const float BASE_SCALE = 0.0010f;
+
+        [Slider("Scale min", -5, 0, DefaultValue = SCALE_MIN)]
+        public int ScaleMin = SCALE_MIN;
+
+        [Slider("Scale max", 0, 20, DefaultValue = SCALE_MAX)]
+        public int ScaleMax = SCALE_MAX;
+
+        [Slider("Scale step", 0.0001f, 0.0009f, DefaultValue = SCALE_STEP, Format = "{0:F4}", Step = 0.0001f)]
+        public float ScaleStep = SCALE_STEP;
+
+        [Slider("Base scale", 0.0005f, 0.0090f, DefaultValue = BASE_SCALE, Format = "{0:F4}", Step = 0.0001f)]
+        public float BaseScale = BASE_SCALE;
+    }
+}
---
+++ BiggerSigns/Patches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace BiggerSigns;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPatch(typeof(uGUI_SignInput), nameof(uGUI_SignInput.UpdateScale))]
+    public static class Patch_uGUI_SignInput_UpdateScale
+    {
+        private static void Prefix(uGUI_SignInput __instance)
+        {
+            __instance.baseScale = Plugin.ModConfig.BaseScale;
+            __instance.scaleStep = Plugin.ModConfig.ScaleStep;
+        }
+    }
+
+    [HarmonyPatch(typeof(uGUI_SignInput), nameof(uGUI_SignInput.scaleIndex))]
+    [HarmonyPatch(MethodType.Setter)]
+    public static class Patch_uGUI_SignInput_scaleIndex_Setter
+    {
+        private static void Prefix(uGUI_SignInput __instance)
+        {
+            __instance.scaleMin = Plugin.ModConfig.ScaleMin;
+            __instance.scaleMax = Plugin.ModConfig.ScaleMax;
+        }
+    }
+}
